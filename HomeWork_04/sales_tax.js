0c1b82ae3b66fbd4a1954dc34890b05ea6663359
lxyqaq <lxyqaq0128@gmail.com>
2020-10-15 15:28:04 +0100

    homework

--- HomeWork_04/sales_tax.js
+++ HomeWork_04/sales_tax.js
@@ -1,5 +1,20 @@
-var $ = function (id) {
-    return document.getElementById(id);
-};
+window.onload = function () {
+
+    var subtotal = document.getElementById("subtotal");
+
+    var tax_rate = document.getElementById("tax_rate");
+
+    var sales_tax = document.getElementById("sales_tax")
+
+    var total = document.getElementById("total");
+
+    var calculate = document.getElementById("calculate");
+
+    var clear = document.getElementById("clear");
+
+    calculate.onclick = function () {
+        
+    }
+}
 
 
